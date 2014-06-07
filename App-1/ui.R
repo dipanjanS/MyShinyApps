4cab38a5c7686dec0ead0e43eeedc1948fa5c764
Dipanjan Sarkar <dipanzan.sarkar@gmail.com>
2014-06-07 19:11:47 +0530

    Added app 1

---
+++ App-1/ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Hello World!"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 5,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
