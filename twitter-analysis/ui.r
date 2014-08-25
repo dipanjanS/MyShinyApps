88cc0a75368031ab39e92db96d1d835dcb4449a7
Dipanjan Sarkar <dipanzan.sarkar@gmail.com>
2014-08-25 21:32:21 +0530

    Update ui.r

--- twitter-analysis/ui.r
+++ twitter-analysis/ui.r
@@ -29,20 +29,16 @@ shinyUI(fluidPage(
     progressInit(),
     tabsetPanel(
           
-      #Output from tab 1 ----So a box plot to see the distribution of scores of sentiments 
+      #Output from tab 4 ----So a box plot to see the distribution of scores of sentiments 
       tabPanel("Sentiment Analysis", plotOutput("sentiboxplot"), HTML
-               ("<div> This plot shows the distribution of positive/negative sentiments about each entity.(Note that tweets were 
-cleaned before this analysis was performed.) For each tweet, a 
-net score of positive and negative sentiments is computed and this plot shows the distribution of scores. 
-Boxplots give information on the minimum, maximum, and 25th, 50th (median), and 75th percentile of the distribution. Blue dots are mean sentiment scores for the 
-two entities. A higher sentiment score suggests more positive (or a less negative) discussion of that entity than the other. A sampling of how these were coded is given below.</div>"),
+               ("<div> This plot shows the distribution of positive/negative sentiments about each entity. Note that tweets were cleaned before this analysis was performed. For each tweet, a net score of positive and negative sentiments are computed and this plot shows the distribution of scores.A higher sentiment score suggests more positive (or a less negative) discussion of that entity than the other.</div>"),
                tableOutput("sentiheadtable"),tableOutput("sentitailtable"),id="test"),
       
-      #Output from tab 2 - Word clouds 
+      #Output from tab 5 - Word clouds - with some html tags
       
       tabPanel("Word Clouds",h2(textOutput("entity1wc")),plotOutput("entity1wcplot"),h2(textOutput("entity2wc")),plotOutput("entity2wcplot")),
       
-      #Output from tabs 3 and 4, the raw tweets
+      #Output from tabs 6 and 7, the raw tweets
       tabPanel("Entity 1 Raw tweets",tableOutput("tableentity1")),
       tabPanel("Entity 2 Raw tweets",tableOutput("tableentity2"))
     )
